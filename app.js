b9fcf1bedb872c723248636460a560e2441d44da
Sofia <sofia.dionisio94@gmail.com>
2020-09-13 12:25:10 +0100

    Assigned functions moveDown, moveLeft and moveRight to keycodes in order to control tetrominoes

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ document.addEventListener("DOMContentLoaded", () => {
   ];
 
   const tTetromino = [
-    [1, width, width + 1, width * 2],
+    [1, width, width + 1, width + 2],
     [1, width + 1, width + 2, width * 2 + 1],
     [width, width + 1, width + 2, width * 2 + 1],
     [1, width, width + 1, width * 2 + 1],
@@ -73,13 +73,29 @@ document.addEventListener("DOMContentLoaded", () => {
   //Tetromino moves down every 1sec
   timerId = setInterval(moveDown, 1000);
 
+  //Assign functions to keycodes
+  function control(e) {
+    if (e.keyCode === 37) {
+      moveLeft();
+    }else if(e.keyCode === 38) {
+      //rotate();
+    }else if(e.keyCode === 39) {
+      moveRight();
+    }else if(e.keyCode === 40) {
+      moveDown();
+    }
+  }
+  document.addEventListener("keydown", control);
+
+  //Move tetromino down
   function moveDown() {
+    freeze();
     undraw();
     currentPosition += width;
     draw();
-    freeze();
   }
 
+  //Freezes tetromino
   function freeze() {
     if (
       current.some((index) =>
@@ -95,5 +111,43 @@ document.addEventListener("DOMContentLoaded", () => {
       currentPosition = 4;
       draw();
     }
+  }
+
+  //Move tetromino left
+  function moveLeft() {
+    undraw();
+    const isAtLeftEdge = current.some(
+      (index) => (currentPosition + index) % width === 0
+    );
+
+    if (!isAtLeftEdge) currentPosition -= 1;
+
+    if (
+      current.some((index) =>
+        squares[currentPosition + index].classList.contains("taken")
+      )
+    ) {
+      currentPosition += 1;
+    }
+    draw();
+  }
+
+  //Move tetromino right
+  function moveRight() {
+    undraw();
+    const isAtRightEdge = current.some(
+      (index) => (currentPosition + index) % width === width-1
+    );
+
+    if (!isAtRightEdge) currentPosition += 1;
+
+    if (
+      current.some((index) =>
+        squares[currentPosition + index].classList.contains("taken")
+      )
+    ) {
+      currentPosition -= 1;
+    }
+    draw();
   }
 });
